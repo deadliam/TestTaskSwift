96b8b7b69b5dee9b8a7d099d368d38f11f12856c
Anatoly Kasyanov <tommy@macpaw.com>
2017-10-10 17:31:38 +0300

    Initial Commit

---
+++ TestTask/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestTask
+//
+//  Created by Anatoly Kasyanov on 10/10/17.
+//  Copyright © 2017 MacPaw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
